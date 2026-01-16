bed75f0303a760fabc25a1dc7a5df84bed11117c
Jauhar azmi <jauhar.azmi.edu@gmal.com>
2026-01-16 11:03:41 +0700

    update1

--- api/admin/logout.php
+++ api/admin/logout.php
@@ -1,9 +1,13 @@
 <?php
-// Hapus semua cookie login
-setcookie("user_login", "", time() - 3600, "/");
+session_start();
+session_destroy(); // Menghapus semua session di server
+
+// 1. Hapus semua cookie login (Nama harus sama persis dengan login_proses.php)
+setcookie("user_name", "", time() - 3600, "/");   // Tadi di login_proses namanya user_name
 setcookie("user_level", "", time() - 3600, "/");
 setcookie("user_status", "", time() - 3600, "/");
 
-header("location:index.php?pesan=logout");
+// 2. Keluar satu folder (../) untuk menemukan index.php (halaman login)
+header("location:../index.php?pesan=logout");
 exit;
 ?>
